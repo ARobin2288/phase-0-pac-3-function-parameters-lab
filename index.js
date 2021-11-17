c2cf0eee538a3b27555fd948fcdeccde2b5a8abc
Robin Andrews <RobinAndrews22@gmail.com>
2021-11-17 12:48:07 -0600

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+function introduction(name) {
+  return `Hi, my name is ${name}.`;
+}
+
+introduction("Nancy");
+
+function introductionWithLanguage(name, language) {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+introductionWithLanguage("Nancy", "React");
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+introductionWithLanguageOptional("Gracie", "Python");
